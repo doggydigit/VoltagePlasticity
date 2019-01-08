69e76ba0e83affce00e44e812c67be5dcdebdb56
doggydigit <matthias.chinyen.tsai@gmail.com>
2019-01-08 14:52:16 +0100

    Increased MC iteration number

--- src/montesearch.py
+++ src/montesearch.py
@@ -166,7 +166,7 @@ def main(protocol_type='Letzkus', plasticity='Claire', veto=False, debug=False, 
 
     # Initialize some variable
     current_score = sys.maxsize
-    nr_iterations = 1000000
+    nr_iterations = 10000000
 
     print('\nStarting Monte-Carlo optimization:')
 
